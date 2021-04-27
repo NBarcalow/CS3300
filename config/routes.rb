175c313427789b58eb0981d3295d8e91ede63785
root <nbarcalo@uccs.edu>
2021-04-27 11:09:17 +0000

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'projects#index'
+  #root to: 'posts#index'
   resources :projects
-  root to: 'posts#index'
-  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
