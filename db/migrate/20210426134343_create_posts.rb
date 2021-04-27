bea7af7a675a1355561fe18a446ab217c43e212c
root <nbarcalo@uccs.edu>
2021-04-27 10:53:05 +0000

    projects

--- db/migrate/20210426134343_create_posts.rb
+++ db/migrate/20210426134343_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.String :title
+      t.string :title
       t.text :body
       t.text :description
       t.string :slug
