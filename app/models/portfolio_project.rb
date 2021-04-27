14e803d31a36291a240763bd5a2c32ec08c9a755
root <nbarcalo@uccs.edu>
2021-04-27 12:24:40 +0000

    Heroku with working page

--- app/models/portfolio_project.rb
+++ app/models/portfolio_project.rb
@@ -1,9 +1,8 @@
 class PortfolioProject < ApplicationRecord
-    #extend FriendlyId
-    #friendly_id :title, use: :slugged
+    extend FriendlyId
+    friendly_id :title, use: :slugged
 
-    #def should_generate_new_friendly_id?
-        #title_changed?
-    #end
-    
+    def should_generate_new_friendly_id?
+        title_changed?
+    end
 end
